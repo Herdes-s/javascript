6b5a30ca85351982e79e21b5f8c3c182c3092e33
Herdes-s <soaressantos02000@gmail.com>
2025-06-30 19:55:57 -0300

    new fim

--- aula141ex/script.js
+++ aula141ex/script.js
@@ -5,15 +5,30 @@ function contar () {
     let res = document.getElementById ('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = 'Impossivel contar! </br>'
         alert('[ERRO] falta dados!')
+
     } else {
         res.innerHTML = 'Contando:' 
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
-        for (let c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
+        if (p <= 0) {
+            alert('Passo invalido! adicionando passo 1!')
+            p = 1
         }
+        if (i < f) {
+            // contagem crescente
+            for (let c = i; c <= f; c += p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }
+            // contagem regressiva
+        }else { 
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449} `
+            }    
+        }
+        res.innerHTML += '\u{1F3C1}'
     }
 }
