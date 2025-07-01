53c16ba5bb4bd2612b8cd0efc44c3e36f4e4abe1
Herdes-s <soaressantos02000@gmail.com>
2025-07-01 11:49:14 -0300

    new2

---
+++ aula142ex/script.js
@@ -0,0 +1,11 @@
+function gerar () {
+    let num = document.getElementById ('txtn')
+    let tab = document.getElementById ('tab')
+    
+    if (num.value.length == 0) {
+        alert('Digite um numero!')
+    }else {
+        let n = Number(num.value)
+    }
+    
+}
